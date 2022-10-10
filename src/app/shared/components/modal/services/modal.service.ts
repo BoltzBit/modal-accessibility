6791906bf9d2ea43b09436773456234bacf6f7e2
mateus.silva <mateus.silva@iatec.com>
2022-10-10 09:40:13 -0300

    Add body-injector

--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, ViewContainerRef } from "@angular/core";
+import { ComponentRef, Injectable, ViewContainerRef } from "@angular/core";
+import { BodyInjectorService } from "src/app/shared/service/body-injector.service";
 import { ModalConfig } from "../interfaces/modal-config";
 import { ModalComponent } from "../modal.component";
 
@@ -6,18 +7,30 @@ import { ModalComponent } from "../modal.component";
 
 export class ModalService{
     
-    constructor(private viewContainerRef: ViewContainerRef){}
+    constructor(
+        private _bodyInjectorService: BodyInjectorService,
+        private _viewContainerRef: ViewContainerRef){}
 
     public open(modalConfig: ModalConfig): ModalRef{
         //componente pode ser fabricado diretamento pela viewContainerRef
-        const componentRef = this.viewContainerRef.createComponent(ModalComponent);
+        const componentRef = this.createComponentRef();
 
-        return new ModalRef();
+        componentRef.instance.modalConfig = modalConfig;
+
+        this._bodyInjectorService.stackBeforeAppRoot(componentRef);
+
+        return new ModalRef(componentRef);
+    }
+
+    private createComponentRef(): ComponentRef<ModalComponent>{
+        return this._viewContainerRef.createComponent(ModalComponent);
     }
 }
 
 export class ModalRef{
+    constructor(private _componentRef: ComponentRef<ModalComponent>){}
     public close(): void{
         console.log('close called');
+        this._componentRef.destroy();
     }
 }
